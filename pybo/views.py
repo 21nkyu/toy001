02aaeae7280e81a45eec58299043b6b689fa5c94
21nkyu <adele7178@naver.com>
2021-10-30 00:00:00 +0900

    2-06

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from django.utils import timezone
+
 from pybo.models import Question
 
 
@@ -21,3 +23,15 @@ def detail(request, question_id):
     question = Question.objects.get(id=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    :param request:
+    :param question_id:
+    :return:
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
