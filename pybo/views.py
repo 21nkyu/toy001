bfb5ffc7d77faf9e81d73940c57369a6377b0bd9
21nkyu <adele7178@naver.com>
2021-11-01 21:51:08 +0900

    2-09(1)

--- pybo/views.py
+++ pybo/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 from django.utils import timezone
 
+from pybo.forms import QuestionForm
 from pybo.models import Question
 
 
@@ -35,3 +36,22 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
+
+def question_create(request):
+    """
+    pybo 질문등록
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+        return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
